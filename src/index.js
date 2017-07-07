72f2749efafca621d1bf973add8f09ee93bbf487
Jay Ullman <jayullman@gmail.com>
2017-07-06 22:15:08 -0700

    Create initial React boilerplate and config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/style.css';
+
+ReactDOM.render(<h1>Hello!!</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract('css-loader')
+      }
+      /* for images
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: 'file-loader?name=[name].[ext]',
+        include: path.join(__dirname, '/client/assets')
+      }
+      */
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      template: 'src/template.html'
+    })
+  ]
+}
+
+/* For Production
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+*/
+module.exports = config;
